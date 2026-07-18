78324c923f9a837bb79287b3295fe7b0157f1bc0
Sebastian Claus <sbstncls@gmail.com>
2026-07-19 00:49:44 +0200

    refactor: moved  to own block in ucode

--- ucode/urom_def.asm
+++ ucode/urom_def.asm
@@ -15,11 +15,14 @@ SIG_PC_OUT  = 1 << SIG_BUS_OFFSET + 0 ; PC to bus
 SIG_MAR_IN  = 1 << SIG_BUS_OFFSET + 1 ; MAR from bus
 SIG_MDR_OUT = 1 << SIG_BUS_OFFSET + 2 ; MDR to bus
 SIG_IR_IN   = 1 << SIG_BUS_OFFSET + 3 ; IR from bus
-SIG_PC_INC  = 1 << SIG_BUS_OFFSET + 4 ; PC increment
-SIG_ACC_IN  = 1 << SIG_BUS_OFFSET + 5 ; ACC from bus
+SIG_ACC_IN  = 1 << SIG_BUS_OFFSET + 4 ; ACC from bus
+
+; PC CONTROL
+SIG_PC_OFFSET = SIG_BUS_OFFSET + 5
+SIG_PC_INC  = 1 << SIG_PC_OFFSET + 0 ; PC increment
 
 ; RAM CONTROL
-SIG_RAM_OFFSET = SIG_BUS_OFFSET + 6 ; offset of ram control signals
+SIG_RAM_OFFSET = SIG_PC_OFFSET + 1 ; offset of ram control signals
 SIG_RAM_READ = 1 << SIG_RAM_OFFSET + 0 ; read from RAM (to MDR)
 
 
