f79e0463aa01047f7faab2efdeda7dea927b106e
Sebastian Claus <sbstncls@gmail.com>
2026-07-21 00:04:50 +0200

    refactor: add label to fetch ucode

--- ucode/urom_fetch.asm
+++ ucode/urom_fetch.asm
@@ -1,8 +1,10 @@
 ; Just the Fetch-Phase
 
+#once
+
 #include "urom_def.asm"
 
-uc SIG_PC_OUT | SIG_MAR_IN ; MAR <- PC
-uc SIG_RAM_READ ; MBR <- ram
-uc SIG_MDR_OUT | SIG_IR_IN | SIG_PC_INC ; IR <- MDR, PC <- PC + 1
- 
+FETCH:
+    uc SIG_PC_OUT | SIG_MAR_IN ; MAR <- PC
+    uc SIG_RAM_READ ; MBR <- ram
+    uc SIG_MDR_OUT | SIG_IR_IN | SIG_PC_INC ; IR <- MDR, PC <- PC + 1
