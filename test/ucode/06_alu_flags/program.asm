e5e6610a0ccf236302194b326943ce0e40583cfc
Sebastian Claus <sbstncls@gmail.com>
2026-07-26 18:31:49 +0200

    test: ADC

--- test/ucode/06_alu_flags/program.asm
+++ test/ucode/06_alu_flags/program.asm
@@ -8,4 +8,28 @@ clc
 sez
 clz
 
+sec
+ldi  0x01
+adci 0x01
+;; should give: 0x03
+
+
+sec
+ldi 0x01
+adc foobar
+;; should give: 0x44
+
+clc
+ldi  0x01
+adci 0x01
+;; should give: 0x02
+
+clc
+ldi 0x01
+adc foobar
+;; should give: 0x43
 halt
+
+halt
+
+foobar: #d16 0x42
