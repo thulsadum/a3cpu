e0c7a020227029e1a64209bebf5232251cf6ed93
Sebastian Claus <sbstncls@gmail.com>
2026-07-26 17:49:21 +0200

    feat: `sez` and `clz`

--- ucode/urom_op_alu_flags.asm
+++ ucode/urom_op_alu_flags.asm
@@ -3,8 +3,11 @@
 #include "urom_op_simple.asm"
 #include "../asmdef/ac3puasm_opcodes.asm"
 
+
 ;;;
 ;;; sec / clc --- carry flag manipulation
+;;;
+
 #bank acpu
 op_sec:
     uc set_flag(SIG_FLAG_SEL_CARRY,1) | SIG_UPC_RESET
@@ -16,3 +19,21 @@ op_clc:
 #d16 op_sec
 #addr OC_FLAG_CARRY_0
 #d16 op_clc
+
+
+
+;;;
+;;; sez / clz --- zero flag manipulation
+;;;
+
+#bank acpu
+op_sez:
+    uc set_flag(SIG_FLAG_SEL_ZERO,1) | SIG_UPC_RESET
+op_clz:
+    uc set_flag(SIG_FLAG_SEL_ZERO,0) | SIG_UPC_RESET
+
+#bank mrom
+#addr OC_FLAG_ZERO_1
+#d16 op_sez
+#addr OC_FLAG_ZERO_0
+#d16 op_clz
