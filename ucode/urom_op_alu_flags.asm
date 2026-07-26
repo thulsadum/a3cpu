62286207a7b6df0943cb55c9d18e7f6b0969ba09
Sebastian Claus <sbstncls@gmail.com>
2026-07-26 17:40:20 +0200

    feat:

--- ucode/urom_op_alu_flags.asm
+++ ucode/urom_op_alu_flags.asm
@@ -8,7 +8,11 @@
 #bank acpu
 op_sec:
     uc set_flag(SIG_FLAG_SEL_CARRY,1) | SIG_UPC_RESET
+op_clc:
+    uc set_flag(SIG_FLAG_SEL_CARRY,0) | SIG_UPC_RESET
 
 #bank mrom
 #addr OC_FLAG_CARRY_1
 #d16 op_sec
+#addr OC_FLAG_CARRY_0
+#d16 op_clc
