735c0cca7a7100cd4e06db67bfa11057fe8c74aa
Sebastian Claus <sbstncls@gmail.com>
2026-07-26 11:03:23 +0200

    test: ALU test cases logic operations

--- Makefile
+++ Makefile
@@ -21,7 +21,7 @@ ucode/%.bin: ucode/%.asm ucode/urom_def.asm
 
 test: test-ucode
 
-test-ucode: test-ucode-01_decode test-ucode-02_pipe test-ucode-03_load_store test-ucode-04_alu_arithmetic
+test-ucode: test-ucode-01_decode test-ucode-02_pipe test-ucode-03_load_store test-ucode-04_alu_arithmetic test-ucode-05_alu_logic
 
 test-ucode-%: all
 	@echo "Testing ucode $* ..."
---
+++ test/ucode/05_alu_logic/program.asm
@@ -0,0 +1,48 @@
+#include "../../../asmdef/ac3puasm_def.asm"
+
+#bank ac3pu_program
+
+ldi 0x8001
+andi 0x8000
+
+ldi 0x8001
+and mask
+
+lda mask
+andi 0x1234
+
+lda mask
+and mask
+
+halt
+
+ldi 0x8001
+ori 0x8000
+
+ldi 0x8001
+or mask
+
+lda mask
+ori 0x1234
+
+lda mask
+or mask
+
+halt
+
+ldi 0x8001
+xori 0x8000
+
+ldi 0x8001
+xor mask
+
+lda mask
+xori 0x1234
+
+lda mask
+xor mask
+
+halt
+
+mask:
+    #d16 0xff00
