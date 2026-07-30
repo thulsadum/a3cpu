7a99090c90d314367b28056bc48ddabdc26dec20
Sebastian Claus <sbstncls@gmail.com>
2026-07-31 00:28:22 +0200

    test: indirect addressing

--- Makefile
+++ Makefile
@@ -50,7 +50,9 @@ test-ucode: test-ucode-01_decode \
 			test-ucode-12_full_zp \
 			test-ucode-13_dynamic_long_word_selection \
 			test-ucode-14_jump \
-			test-ucode-15_jumpz
+			test-ucode-15_jumpz \
+			test-ucode-16_indirect_addressing
+#			test-ucode-17_branching
 
 test-ucode-%: all
 	@echo "Testing ucode $* ..."
---
+++ test/ucode/16_indirect_addressing/program.asm
@@ -0,0 +1,47 @@
+#include "../../../asmdef/ac3puasm_def.asm"
+
+#bank ac3pu_program
+
+
+program:
+    lda ptr
+    lia  ; load indirect by ACC
+    xori 0xC0FF ;; expected: ACC = 0x0000, flags: zero
+
+    lda ptr
+    inc
+    lia
+    xori 0xBEEF ;; expected: ACC = 0x0000, flags: zero
+
+    ldia ptr
+    xori 0xC0FF ;; expected: ACC = 0x0000, flags: zero
+
+    ldi  0x1337
+    stia ptr
+
+    ldia ptr_far
+    lia  ; this double deref
+    xori 0x1337 ;; expected: ACC = 0x0000, flags: zero
+
+
+    ldia ptr_far_target
+    xori 0xffff         ;; expect succsss.
+
+    ldi  0x4242
+    stia ptr_far_target
+    ldia ptr_far_target
+    xori 0x4242         ;; expected: ACC = 0x0000, flags: zero
+
+halt
+
+#addr 0x7f
+ptr: #d16 var
+#addr 0x8f
+var: #d16 0xC0FF, 0xBEEF
+
+#addr 0x100
+ptr_far: #d16 ptr
+
+#addr 0x200
+#d16 0xffff
+ptr_far_target: #d16 $-1
