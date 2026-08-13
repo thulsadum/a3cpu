077b6a00f8fa6f2c14c426f6b7c522d50e738cb6
Sebastian Claus <sbstncls@gmail.com>
2026-08-13 23:13:53 +0200

    feat(forth): add simple token stream pattern optimizations.

--- forth/parsing/optimizer.py
+++ forth/parsing/optimizer.py
@@ -1,7 +1,26 @@
-
+from .tokens import *
 
 class Optimizer:
 
     def optimize(self, tokens):
-        return tokens
+        result = []
+        for token in tokens:
+            result.append(token)
+            match result[-3:]:
+                case [LiteralToken(a), LiteralToken(b), WordToken("+")]:
+                    result[-3:] = [LiteralToken(a+b)]
+                case [LiteralToken(a), LiteralToken(b), WordToken("-")]:
+                    result[-3:] = [LiteralToken(a-b)]
+                case [LiteralToken(a), LiteralToken(b), WordToken("SHL")]:
+                    result[-3:] = [LiteralToken(a<<b)]
+                case [LiteralToken(a), LiteralToken(b), WordToken("SHR")]:
+                    result[-3:] = [LiteralToken(a>>b)]
+                case [LiteralToken(a), LiteralToken(b), WordToken("AND")]:
+                    result[-3:] = [LiteralToken(a & b)]
+                case [LiteralToken(a), LiteralToken(b), WordToken("OR")]:
+                    result[-3:] = [LiteralToken(a | b)]
+                case [LiteralToken(a), LiteralToken(b), WordToken("XOR")]:
+                    result[-3:] = [LiteralToken(a ^ b)]
+
+        return result
 
