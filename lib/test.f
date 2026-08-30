c358ce8d5429fd362d00c572361591b2d9329179
Sebastian Claus <sbstncls@gmail.com>
2026-08-30 12:18:36 +0200

    fix(forth/lib): fix test word implementations.

--- lib/test.f
+++ lib/test.f
@@ -5,30 +5,34 @@ VARIABLE actual-depth
 VARIABLE cursor
 CREATE actual-results 20 CELLS ALLOT
 
-: T{ ( -- ) 
-\ save the stack depth before test
-    DEPTH start-depth !   ;
+: T{ ( -- )
+   DEPTH start-depth ! ;
 
+: -> ( ... -- )
+   DEPTH DUP actual-depth !
+   start-depth @ > IF
+      actual-depth @ start-depth @ - cursor !
+      BEGIN
+         cursor @ 1- cursor !
+         actual-results cursor @ CELLS + !
+         cursor @ 0=
+      UNTIL
+   THEN ;
 
-: -> ( ... -- ) 
-\ save all values since test beginning to RAM
-    DEPTH       ( -- d )
-       DUP actual-depth !   ( -- d)
-       start-depth @ > IF   ( -- )
-          actual-depth @  start-depth @   -   cursor !      ( -- )
-          BEGIN
-             cursor @ 1- cursor !             ( -- )
-             actual-results cursor @ + !      ( x -- ) \ Sichert direkt an cursor-offset
-             cursor @ 0=   UNTIL   THEN   ;
-
-
-: }T ( ... -- ) 
-\ assert expected stack depth and expected values
-
-   DEPTH  start-depth @ -   actual-depth @  start-depth @ - DUP cursor !  = NOT IF
-      0xf1 FAIL THEN
-      cursor @  IF   BEGIN
-         cursor @ 1- cursor !                 ( -- )
-         actual-results cursor @ + @          ( -- actual-results[c] )
-         = NOT   IF   0xf2 FAIL  THEN
-         cursor @ 0=   UNTIL THEN  ;
+: }T ( ... -- )
+   DEPTH start-depth @ -   actual-depth @ start-depth @ -   = IF 
+      actual-depth @  start-depth @  -  DUP  cursor ! IF
+         BEGIN
+            cursor @ 1- cursor !
+            actual-results cursor @ CELLS + @
+            = IF
+               \ Match -> Weiter
+            ELSE
+               0xf2 FAIL
+            THEN
+            cursor @ 0=
+         UNTIL
+      THEN
+   ELSE
+      0xf1 FAIL
+   THEN ;
