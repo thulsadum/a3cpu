ba0423560472dddc8bbb61c09a23a2c5e525526a
Sebastian Claus <sbstncls@gmail.com>
2026-08-30 12:24:41 +0200

    test(forth/test): add some test cases that should fail.

--- test/forth/0f_test_harness/program.f
+++ test/forth/0f_test_harness/program.f
@@ -5,6 +5,8 @@ REQUIRE lib/test.f
 T{ -> }T \ simplest test case
 
 \ T{ 0xBAD -> }T \ should fail
+\ T{ -> 0xBAD }T \ should fail
+\ T{ 1 1 + -> 3 }T \ should fail
 
  ( 40 2   -- 42 )
 T{ 40 2 + -> 42 }T
