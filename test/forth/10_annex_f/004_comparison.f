b2de0efc8636d9ec1ea34033bf7398af9c78a5ee
Sebastian Claus <sbstncls@gmail.com>
2026-09-03 23:34:59 +0200

    test(forth): add test cases for test harness `0=` and `=`.

---
+++ test/forth/10_annex_f/004_comparison.f
@@ -0,0 +1,21 @@
+
+\ F.6.1.0270
+\ 0=
+T{        0 0= -> <TRUE>  }T
+T{        1 0= -> <FALSE> }T
+T{        2 0= -> <FALSE> }T
+T{       -1 0= -> <FALSE> }T
+T{ MAX-UINT 0= -> <FALSE> }T
+T{ MIN-INT  0= -> <FALSE> }T
+T{ MAX-INT  0= -> <FALSE> }T
+
+
+\ F.6.1.0530
+\ =
+T{  0  0 = -> <TRUE>  }T
+T{  1  1 = -> <TRUE>  }T
+T{ -1 -1 = -> <TRUE>  }T
+T{  1  0 = -> <FALSE> }T
+T{ -1  0 = -> <FALSE> }T
+T{  0  1 = -> <FALSE> }T
+T{  0 -1 = -> <FALSE> }T
--- test/forth/10_annex_f/program.f
+++ test/forth/10_annex_f/program.f
@@ -6,5 +6,6 @@ REQUIRE 000_basic_assumptions.f
 REQUIRE 001_logic.f
 REQUIRE 002_shifts.f
 REQUIRE 003_numerics.f
+REQUIRE 004_comparison.f
 
 [ASM halt ]
